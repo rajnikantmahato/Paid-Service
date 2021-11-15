bbdce34804d84d6353a3ab1c895925151622b6f9
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2021-11-15 21:39:41 +0530

    Add files via upload

---
+++ topnav.js
@@ -0,0 +1,11 @@
+// nav bar start
+function myFunction() {
+    var x = document.getElementById("RTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+// nav bar end
+
